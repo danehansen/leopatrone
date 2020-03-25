4d90ef23a96a682db7e311d49090067c98c33752
Dane Hansen <dane@danehansen.com>
2020-03-24 20:31:35 -0700

    add react scripts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi! My name is Leonardo Patrone! And this is my new website!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
