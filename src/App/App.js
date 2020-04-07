a31c218ed13a2398c203e3139b2191005772dd3d
Dane Hansen <dane@danehansen.com>
2020-04-06 18:02:05 -0700

    make a demo gallery

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 import { Link } from "react-router-dom";
 
 export default function App({children}) {
   return (
        <div>
-       <ul>
-         <li>
-           <Link to="/">Home</Link>
-         </li>
-         <li>
-           <Link to="/wedding1">Wedding 1</Link>
-         </li>
-         <li>
-           <Link to="/wedding2">Wedding 2</Link>
-         </li>
-       </ul>
+       <nav className={styles.nav}>
+           <Link className={styles.link} to="/">Home</Link>
+           <Link className={styles.link} to="/wedding1">Wedding 1</Link>
+           <Link className={styles.link} to="/wedding2">Wedding 2</Link>
+       </nav>
          {children}
        </div>
    );
--- src/pages/Wedding1/Wedding1.js
+++ src/pages/Wedding1/Wedding1.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import styles from './Wedding1.module.css';
+
+const IMAGES = [
+  'Jenzack0017.jpg',
+  'Jenzack0082.jpg',
+  'Jenzack0115.jpg',
+  'Jenzack0125.jpg',
+  'Jenzack0175.jpg',
+  'Jenzack0177.jpg',
+  'Jenzack0189.jpg',
+  'Jenzack0191.jpg',
+  'Jenzack0216.jpg',
+  'Jenzack0268.jpg',
+  'Jenzack0272.jpg',
+  'Jenzack0274.jpg',
+  'Jenzack0294.jpg',
+  'Jenzack0412.jpg',
+  'Jenzack0420.jpg',
+  'Jenzack0424.jpg',
+  'Jenzack0431.jpg',
+  'Jenzack0432.jpg',
+  'Jenzack0500.jpg',
+  'Jenzack0549.jpg',
+  'Jenzack0562.jpg',
+  'Jenzack0563.jpg',
+  'Jenzack0568.jpg',
+  'Jenzack0574.jpg',
+  'Jenzack0612.jpg',
+  'Jenzack0627.jpg',
+  'Jenzack0793.jpg',
+  'Jenzack0795.jpg',
+  'Jenzack0809.jpg',
+];
 
 export default function Wedding1() {
   return (
        <div>
-         <h1>wedding gallery 1</h1>
+         {IMAGES.map(function(image) {
+           return <img className={styles.img} src={require(`./gallery30/${image}`)} />
+         })}
        </div>
    );
 }
