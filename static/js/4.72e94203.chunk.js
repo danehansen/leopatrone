5cf8351f4405825f49dcfe91bd7674d056d4200d
Dane Hansen <dane@danehansen.com>
2020-04-06 19:18:27 -0700

    Updates

---
+++ static/js/4.72e94203.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpleopatrone=this.webpackJsonpleopatrone||[]).push([[4],{63:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return o}));var a=t(0),r=t.n(a);function o(){return r.a.createElement("div",null,r.a.createElement("h1",null,"home page"))}}}]);
+//# sourceMappingURL=4.72e94203.chunk.js.map
